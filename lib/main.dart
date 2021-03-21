3eb38f1ab5d3b672e742499875b24526dcf27b0e
LucasGarcez <lucasgarcezdev@gmail.com>
2021-03-21 19:33:37 -0300

    my first Flutter App :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/perfil.jpeg'),
+              ),
+              Text(
+                'Lucas Garcez',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 40, fontFamily: 'Pacifico'),
+              ),
+              SizedBox(
+                height: 30,
+                width: 150,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    fontFamily: 'Source Sans Pro',
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
+                      '+55 22 98123222',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    )),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Text(
+                    'flutterdev@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
